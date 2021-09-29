b17aab57dbffee73c6324e4e41be4cb98f69a62d
databasetechi <n.sharma0502@gmail.com>
2021-09-30 03:23:32 +0530

    checking login button

---
+++ login.java
@@ -0,0 +1,12 @@
+     	public class Login
+     	{
+     		public static void main(String args[])
+     		{
+     			int a = 50
+     	
+     			for(i=0;i<=40;i++)
+     			{
+     				System.out.println("Hello World!");
+    			}
+    		}
+    	}
