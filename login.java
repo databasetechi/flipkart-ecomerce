4b594a4dfae5b57992d3de49d91f67e8ed46e4d0
databasetechi <n.sharma0502@gmail.com>
2021-11-01 22:50:57 +0530

    still trying

--- login.java
+++ login.java
@@ -2,9 +2,9 @@
      	{
      		public static void main(String args[])
      		{
-     			int a = 60
+     			int a = 60;
      	
-     			for(i=0;i<=60;i++)
+     			for(i=0;i<=100;i++)
      			{
      				System.out.println("Hello World!");
     			}
