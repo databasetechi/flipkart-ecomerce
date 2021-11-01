7e2b2d215645157e2e2e12adb59add004752ec14
databasetechi <n.sharma0502@gmail.com>
2021-11-01 22:58:58 +0530

    CODING

--- login.java
+++ login.java
@@ -2,9 +2,9 @@
      	{
      		public static void main(String args[])
      		{
-     			int a = 60;
+     			int a = 0;
      	
-     			for(i=0;i<=100;i++)
+     			for(i=0;i<=10;i++)
      			{
      				System.out.println("Hello World!");
     			}
